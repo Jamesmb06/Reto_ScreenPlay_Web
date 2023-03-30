f4a9d507318829b1ac9928ab5a8fbbcddf10e19e
James Muñoz <jamb1993@gmail.com>
2023-03-30 14:11:26 -0500

    Esperas explicitas agregadas

--- src/main/java/com/sofkau/tasks/IniciarSesion.java
+++ src/main/java/com/sofkau/tasks/IniciarSesion.java
@@ -4,12 +4,13 @@ import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.waits.WaitUntil;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
-
 import static com.sofkau.ui.PaginaInicial.*;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isClickable;
 
 public class IniciarSesion implements Task {
 
@@ -22,9 +23,11 @@ public class IniciarSesion implements Task {
             throw new RuntimeException(e);
         }
         actor.attemptsTo(
+                WaitUntil.the(BOTON_CLIENTES,isClickable()),
                 Click.on(BOTON_CLIENTES),
                 Enter.theValue(propiedades.getProperty("serenity.username")).into(CAMPO_CORREO),
                 Enter.theValue(propiedades.getProperty("serenity.password")).into(CAMPO_CONTRASENNA),
+                WaitUntil.the(BOTON_INICIAR_SESION,isClickable()),
                 Click.on(BOTON_INICIAR_SESION)
         );
     }
--- src/main/java/com/sofkau/tasks/RealizarCompra.java
+++ src/main/java/com/sofkau/tasks/RealizarCompra.java
@@ -4,20 +4,31 @@ import com.sofkau.ui.ProcesoCompra;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
-
+import net.serenitybdd.screenplay.waits.WaitUntil;
+import static com.sofkau.ui.ProcesoCompra.*;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.*;
 public class RealizarCompra implements Task {
     @Override
     public <T extends Actor> void performAs(T actor) {
 
         actor.attemptsTo(
-                Click.on(ProcesoCompra.CATEGORIA_PRODUCTOS),
-                Click.on(ProcesoCompra.ORDENAR),
-                Click.on(ProcesoCompra.ORDENAR_POR_NOMBRE),
+                WaitUntil.the(CATEGORIA_PRODUCTOS,isClickable()),
+                Click.on(CATEGORIA_PRODUCTOS),
+                WaitUntil.the(ORDENAR,isClickable()),
+                Click.on(ORDENAR),
+                WaitUntil.the(ORDENAR_POR_NOMBRE,isClickable()),
+                Click.on(ORDENAR_POR_NOMBRE),
+                WaitUntil.the(ITEM1,isClickable()),
                 Click.on(ProcesoCompra.ITEM1),
+                WaitUntil.the(CARRITO,isClickable()),
                 Click.on(ProcesoCompra.CARRITO),
+                WaitUntil.the(REALIZAR_PEDIDO,isClickable()),
                 Click.on(ProcesoCompra.REALIZAR_PEDIDO),
+                WaitUntil.the(SIGUIENTE,isClickable()),
                 Click.on(ProcesoCompra.SIGUIENTE),
-                Click.on(ProcesoCompra.PAGO_EFECTIVO)
+                WaitUntil.the(PAGO_EFECTIVO,isClickable()),
+                Click.on(ProcesoCompra.PAGO_EFECTIVO),
+                WaitUntil.the(COMPRAR,isClickable())
                 //Click.on(ProcesoCompra.COMPRAR)
         );
     }
